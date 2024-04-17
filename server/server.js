0e75762a481b183a7c2119dc4e82558c1fd94092
guy33liba <81708094+guy33liba@users.noreply.github.com>
2024-04-17 14:53:56 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,32 +5,38 @@ const mongoose = require("mongoose")
 app.use(cors())
 app.use(express.json())
 
-const mongoUrl =
-  "mongodb+srv://guy33liba:Aa123123@workout.6vzljfv.mongodb.net/?retryWrites=true&w=majority&appName=workout"
-mongoose.connect(mongoUrl)
+ const mongoUrl =
+   "mongodb+srv://guy33liba:Aa123123@workout.6vzljfv.mongodb.net/?retryWrites=true&w=majority&appName=workout"
+ mongoose.connect(mongoUrl)
 
-const commentSchema = new mongoose.Schema({
-  upperBody: String,
-  abs: String,
-  cardio: String,
-  legs: String,
-})
-const Comment = mongoose.model("WorkOut", commentSchema)
-const workoutsComments = []
+ const commentSchema = new mongoose.Schema({
+   upperBody: String,
+   abs: String,
+   cardio: String,
+   legs: String,
+   commentsInput: String,
+ })
+ const Comment = mongoose.model("WorkOut", commentSchema)
+const workoutsCommentsAndLists = []
 
+ app.post("/comments", (req, res) => {
+   const { upperBody, abs, cardio, legs, comments } = req.body
+   const newComment = new Comment({ upperBody, abs, cardio, legs, comments })
+   newComment.save().then((comment) => {
+     workoutsComments.push(comment)
+     res.send(workoutsComments)
+   })
+ })
 app.post("/comments", (req, res) => {
-  const { upperBody, abs, cardio, legs } = req.body
-  const newComment = new Comment({ upperBody, abs, cardio, legs })
-  newComment.save().then((comment) => {
-    workoutsComments.push(comment)
-    res.send(workoutsComments)
-  })
+  const { upperBody, abs, cardio, legs, comments } = req.body
+  workoutsCommentsAndLists.push(upperBody, abs, cardio, legs, comments)
+  res.send(workoutsCommentsAndLists)
 })
-
-app.put("/comments/:id", (req, res) => {
-  const { id } = req.params.id
-  const { upperBody, abs, cardio, legs } = req.body
-  const updatedComment = { upperBody, abs, cardio, legs }
+console.log(workoutsCommentsAndLists)
+ app.put("/comments/:id", (req, res) => {
+   const { id } = req.params.id
+   const { upperBody, abs, cardio, legs } = req.body
+   const updatedComment = { upperBody, abs, cardio, legs }
 
   Comment.findByIdAndUpdate(id, updatedComment, { new: true }, (err, comment) => {
     if (err) {
@@ -41,19 +47,23 @@ app.put("/comments/:id", (req, res) => {
   })
 })
 app.delete("/comments/:id", (req, res) => {
-  const { id } = req.params.id
-  Comment.findByIdAndDelete(id, (req, res) => {
-    res.send(workoutsComments)
-  })
-})
-app.get("/comments", (req, res) => {
-  Comment.find({}, (err, comments) => {
+  const { id } = req.params
+  Comment.findByIdAndDelete(id, (err, deletedComment) => {
     if (err) {
       res.status(500).send(err)
     } else {
-      res.send(comments)
+      res.send(deletedComment)
     }
   })
+})
+app.get("/comments", async (req, res) => {
+  res.send(workoutsCommentsAndLists)
+  try {
+    const comments = await Comment.find({})
+    res.send(comments)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
 app.listen(4000)
