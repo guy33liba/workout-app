d5d76b92e14d1b8eacb6fe81c27aad9afca2088f
guy33liba <81708094+guy33liba@users.noreply.github.com>
2024-04-17 10:16:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react"
+import React, { createContext, useContext, useState } from "react"
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Home from "./workout-app/components/home/Home"
-import UpperBody from "./workout-app/components/upper-body/UpperBody"
-import Legs from "./workout-app/components/legs/Legs"
-import Cardio from "./workout-app/components/cardio/Cardio"
-import Abs from "./workout-app/components/abs/Abs"
+import Home from "./components/home/Home"
+import Legs from "./components/legs/Legs"
+import Abs from "./components/abs/Abs"
+import Cardio from "./components/cardio/Cardio"
+import UpperBody from "./components/upperbody/UpperBody"
+//
+export const WorkOutContextList = createContext()
+export const WorkOutContextComments = createContext()
 
 const App = () => {
+  const [workoutList, setWorkoutList] = useState([])
+  const [workoutComments, setWorkoutComments] = useState([])
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upperbody" element={<UpperBody />} />
-        <Route path="/legs" element={<Legs />} />
-        <Route path="/abs" element={<Abs />} />
-        <Route path="/cardio" element={<Cardio />} />
-      </Routes>
-    </Router>
+    <WorkOutContextComments.Provider value={{ workoutComments, setWorkoutComments }}>
+      <WorkOutContextList.Provider value={{ workoutList, setWorkoutList }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/upperbody" element={<UpperBody />} />
+            <Route path="/legs" element={<Legs />} />
+            <Route path="/abs" element={<Abs />} />
+            <Route path="/cardio" element={<Cardio />} />
+          </Routes>
+        </Router>
+      </WorkOutContextList.Provider>
+    </WorkOutContextComments.Provider>
   )
 }
 
