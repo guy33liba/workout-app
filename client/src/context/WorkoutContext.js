9d847d63e7eb783bf0f55f1f8d3f5eeb07c669ab
Guy Liba <guy33liba@gmail.com>
2024-04-18 13:48:51 +0200

    main

--- client/src/context/WorkoutContext.js
+++ client/src/context/WorkoutContext.js
@@ -2,12 +2,12 @@ import React, { useState, createContext } from "react"
 
 export const WorkingOutContext = createContext()
 const WorkoutContext = ({ children }) => {
+  const [cardio, setCardio] = useState([])
   const [workoutList, setWorkoutList] = useState([])
   const [workoutComments, setWorkoutComments] = useState([])
   const [abs, setAbs] = useState([])
   const [legs, setLegs] = useState([])
   const [upperBody, setUpperBody] = useState([])
-  const [cardio, setCardio] = useState([])
   return (
     <WorkingOutContext.Provider
       value={{
